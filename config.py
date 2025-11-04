6c541d0a0fdf33617f522d3c24929010916f8900
x-ViRKiS-x <55256198+x-ViRKiS-x@users.noreply.github.com>
2025-11-04 17:04:32 +0500

    Update config.py

--- config.py
+++ config.py
@@ -5,3 +5,19 @@ ADMIN_IDS = [356816394]
 # Настройки безопасности
 MAX_REQUESTS_PER_DAY = 50
 REQUEST_TIMEOUT = 30
+
+# Система лимитов
+FREE_DAILY_LIMITS = {
+    'searches': 3,
+    'interviews': 2,
+    'exports': 1
+}
+
+PREMIUM_PRICE = 490  # руб/месяц
+PREMIUM_FEATURES = [
+    "Неограниченный поиск кандидатов",
+    "Расширенная аналитика",
+    "Экспорт в Excel/PDF", 
+    "Шаблоны писем кандидатам",
+    "Приоритетная поддержка"
+]
